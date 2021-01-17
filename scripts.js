8e77c031faf6734ed1d151cac8cd9e4a9e78fe01
lutetia <lutetia@users.noreply.github.com>
2021-01-17 00:20:12 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
   var dataNames = [
-    {"vardas": "Albinas","pavarde":"J","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Marija","pavarde":"J","metai":"1950","menuo":"3","diena":"13",},
-    {"vardas": "Robertas","pavarde":"J","metai":"1981","menuo":"4","diena":"9",},
-    {"vardas": "Elija","pavarde":"J","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Laura","pavarde":"J","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Jolanta","pavarde":"Laz","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Jonas","pavarde":"Laz","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Stase","pavarde":"Vait","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Dovydas","pavarde":"J","metai":"1948","menuo":"2","diena":"25",},
-    {"vardas": "Janyte","pavarde":"J","metai":"1948","menuo":"1","diena":"2",},
-    {"vardas": "Denis","pavarde":"J","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Viktoras","pavarde":"J","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Vytas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Onute","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Vaidas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Kestas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Neilandas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Stefanija","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25",},
-    {"vardas": "Jonas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25",},
+    {"vardas": "Albinas","pavarde":"J","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Marija","pavarde":"J","metai":"1950","menuo":"3","diena":"13","active":"1",},
+    {"vardas": "Robertas","pavarde":"J","metai":"1981","menuo":"4","diena":"9","active":"1",},
+    {"vardas": "Elija","pavarde":"J","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Laura","pavarde":"J","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Jolanta","pavarde":"Laz","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Jonas","pavarde":"Laz","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Stase","pavarde":"Vait","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Dovydas","pavarde":"J","metai":"1948","menuo":"2","diena":"25","active":"1",},
+    {"vardas": "Janyte","pavarde":"J","metai":"1948","menuo":"1","diena":"2","active":"1",},
+    {"vardas": "Denis","pavarde":"J","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Viktoras","pavarde":"J","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Vytas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Onute","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Vaidas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Kestas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Neilandas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25","active":"1",},
+    {"vardas": "Stefanija","pavarde":"Sim","metai":"1948","menuo":"1","diena":"16","active":"",},
+    {"vardas": "Jonas","pavarde":"Sim","metai":"1948","menuo":"10","diena":"25","active":"1",},
    
   ];
   
@@ -70,7 +70,7 @@
     dataNames.forEach(function(dat){
       let d = new Date(currentYear, dat.menuo - 1, dat.diena);
 
-      if (d >= now && d < min)
+      if (dat.active && d >= now && d < min)
       {
         min = d;
         minIndex = index;
